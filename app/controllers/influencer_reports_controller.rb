2bf56597963602b30272c799b8ba52e0fac37e1b
harrisjm <hjmotiwala@utexas.edu>
2018-12-27 05:40:20 -0600

    add strong params to influencer report controller

--- app/controllers/influencer_reports_controller.rb
+++ app/controllers/influencer_reports_controller.rb
@@ -1,2 +1,20 @@
 class InfluencerReportsController < ApplicationController
+
+  
+
+  private
+
+  def influencerreport_params
+    params.require(:influencerreport).permit(:instagram_handle,
+                                             :email,
+                                             :city,
+                                             :post_price_by_category,
+                                             :post_price_by_category_engagements,
+                                             :post_price_by_avg_engagements,
+                                             photos_attributes: [
+                                               :industry,
+                                               :likes,
+                                               :comments
+                                               ])
+  end
 end
