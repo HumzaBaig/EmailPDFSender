649aa1232c7d066282caa0631a83ba510a182f38
harrisjm <hjmotiwala@utexas.edu>
2019-01-01 02:23:21 -0600

    add report_email method to InfluencerReportMailer mailer

--- app/mailers/influencer_report_mailer.rb
+++ app/mailers/influencer_report_mailer.rb
@@ -1,3 +1,11 @@
 class InfluencerReportMailer < ApplicationMailer
   default from: 'Team Datafluencer <datafluencer@gmail.com>'
+
+  def report_email(influencer_report)
+    @influencer_report = influencer_report
+    @influencer = @influencer_report.instagram_handle
+    @influencer_email = @influencer_report.email
+
+    mail(to: `#{@influencer} <#{@influencer_email}>`, bcc: 'Team Datafluencer <datafluencer@gmail.com>', subject: 'Your Instagram Report')
+  end
 end
